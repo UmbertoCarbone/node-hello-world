7f8340322d9d27782910c401436e51ca04022606
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-26 14:18:36 +0200

    aggiunto al json: "watch" e cambiando la parola dentro al console.log cambia anche nel terminale.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+/* FATTO IN JSON
+
+// Crea lo script e farlo partire: ---> 
+
+// "start": "node index.js",
+
+//Crea lo script,fallo partire, cambiando il nostro console.log() cambia la scritta anche nel  terminale --->
+//  "watch": "node --watch index.js"
+
+ */
 console.log("Hello World")
